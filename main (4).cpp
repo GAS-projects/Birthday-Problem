2687e023b4f4d4168828d106df8a010434c151b8
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-03-26 08:53:48 +0900

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  vector<int> birthday;
+  int classmates = 0;
+  cout << "グループの人数を入力してください。" << endl;
+  cin >> classmates;
+  int n = 0;
+  double t = 0;
+
+  srand((unsigned)time(NULL));
+  for(int z = 0; z < 20000; z++){
+    for(int i = 0; i < classmates; i++){
+      birthday.push_back(rand() % 365 + 1);
+    }
+    int count = 0;
+    for(int i = 0; i < 366; i++){
+      for(int j = 0; j < birthday.size() - 1; j++) if(birthday[j] == i) count += 1;
+      if(count > 1) n += 1;
+      count = 0;
+    }
+
+    birthday.clear();
+    if(n > 0) t += 1;
+    n = 0;
+  }
+  cout << "グループの中で誕生日が同じペアが1つ以上できる確率は" << t / 200 << "%でした。" << endl;
+
+  return 0;
+}
