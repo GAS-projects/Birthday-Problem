92ca21c640937c9679d5c4d8af9ea0e29accd304
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-03-28 12:49:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main(){
   vector<int> birthday;
-  int classmates = 0;
-  cin >> classmates;
-  int p = 0;
+  int classmates, p = 0;
   double n = 0;
+  cout << "グループの人数を入力してください。" << endl;
+  cin >> classmates;
 
   srand((unsigned)time(NULL));
-  for(int z = 0; z < 20000; z++){
-    for(int i = 0; i < classmates; i++) birthday.push_back(rand() % 365 + 1);
-    for(int i = 1; i < 366; i++){
-      for(int j = 0; j < birthday.size(); j++) if(birthday[j] == i) p += 1;
+  for(int z = 0; z < 200000; z++){
+    for(int j = 0; j < classmates; j++) birthday.push_back(rand() % 365 + 1);
+    for(int j = 1; j < 366; j++){
+      for(int k = 0; k < birthday.size(); k++) if(birthday[k] == j) p += 1;
       if(p > 1) break;
       else p = 0;
     }
@@ -23,7 +23,7 @@ int main(){
     p = 0;
     birthday.clear();
   }
-  cout << n / 200.0 << "%" << endl;
+  cout << "グループ内に誕生日が同じペアがある確率は" << n / 200000 << "です。" << endl;
   n = 0;
 
   return 0;
